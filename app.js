4fe7ecc2f3b4229a57613bd951462e5832476f4f
Lucas Mace <lucasmace4130@gmail.com>
2020-11-11 12:28:17 -0600

    Squares are populating, labled and wrapping

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // Add Bootstrap CSS Stylesheet with DOM --
 const bootstrap = document.createElement("link");
+const rel = "stylesheet";
 const href = "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css";
 const integrity = "sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO";
 const crossorigin = "anonymous";
 
+bootstrap.rel = rel;
 bootstrap.href = href;
 bootstrap.integrity = integrity;
 bootstrap.crossOrigin = crossorigin;
@@ -11,25 +13,27 @@ bootstrap.crossOrigin = crossorigin;
 document.head.appendChild(bootstrap);
 
 // Gives classes to the <body> --
-document.body.className = "d-flex justify-content-start";
+document.body.className = "container";
 
 // Creates <div> for the <button> --
 const btnDiv = document.createElement("div");
+btnDiv.className = "d-flex justify-content-center"
 document.body.appendChild(btnDiv);
 
 // Creates <button> --
 const btn = document.createElement("button");
 const btnTxt = document.createTextNode("Add Square");
 btn.appendChild(btnTxt);
-btn.className = "flex-item";
-btnDiv.appendChild(btn);
+btn.className = "flex-item btn btn-lg outline-primary";
+btnDiv.appendChild(btn); // Places <button> in btnDiv --
 
 let squareNum = 1;
 
 // Creates <main> for the Squares --
 let myMain = document.createElement("main");
-myMain.className = "d-flex justify-content-start";
+myMain.className = "container d-flex justify-content-start flex-wrap";
 document.body.appendChild(myMain);
+
 
 btn.addEventListener("click", function () {
     let newSquare = document.createElement("div");
