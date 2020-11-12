3d73c601311803fb8dac95019ad7a85dac394007
Lucas Mace <lucasmace4130@gmail.com>
2020-11-11 20:43:15 -0600

    Final Commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Add Bootstrap CSS Stylesheet with DOM --
+// Build Bootstrap CSS Import --
 const bootstrap = document.createElement("link");
 const rel = "stylesheet";
 const href = "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css";
@@ -10,12 +10,13 @@ bootstrap.href = href;
 bootstrap.integrity = integrity;
 bootstrap.crossOrigin = crossorigin;
 
+// Add Bootstrap to the <head> --
 document.head.appendChild(bootstrap);
 
-// Gives classes to the <body> --
+// Gives class to the <body> --
 document.body.className = "container";
 
-// Creates <div> for the <button> --
+// Creates <div> with classes for the <button> --
 const btnDiv = document.createElement("div");
 btnDiv.className = "d-flex justify-content-center"
 document.body.appendChild(btnDiv);
@@ -24,9 +25,10 @@ document.body.appendChild(btnDiv);
 const btn = document.createElement("button");
 const btnTxt = document.createTextNode("Add Square");
 btn.appendChild(btnTxt);
-btn.className = "flex-item btn btn-lg btn-info";
+btn.className = "btn btn-lg btn-info";
 btnDiv.appendChild(btn); // Places <button> in btnDiv --
 
+// Initial State Variable for naming the squares as they populate --
 let squareNum = 1;
 
 // Creates <main> for the Squares --
@@ -34,56 +36,60 @@ let myMain = document.createElement("main");
 myMain.className = "d-flex flex-wrap";
 document.body.appendChild(myMain);
 
-
+// Calls addSquare() on each <button> click --
 btn.addEventListener("click", function () {
     addSquare();
 });
 
 function addSquare() {
+    // Creates newSquare, gives class names, and appends Square to the <main> --
     let newSquare = document.createElement("div");
     newSquare.className = "mySquare d-flex justify-content-center align-items-center";
     newSquare.id = squareNum;
-
     myMain.appendChild(newSquare);
 
+    // Listener for displaying the Square ID --
     newSquare.addEventListener("mouseover", function () {
         newSquare.textContent = newSquare.id;
     });
+    // Listener for removing the displayed ID --
     newSquare.addEventListener("mouseout", function () {
         newSquare.textContent = "";
     });
+    // Listener for changing the background color of the Square --
     newSquare.addEventListener("click", function () {
         newSquare.style.background = bgSwap();
     });
+    
+    // Listener for removing squares --
     newSquare.addEventListener("dblclick", function () {
+        // Get all Squares as elements and put into array on each double click event --
         let totalSquares = document.getElementsByClassName("mySquare");
         let lastSquare = totalSquares.length - 1;
+
+        // Puts the ID of the final square in a variable --
         let lastCheck = totalSquares[lastSquare].id;
 
+        // Puts the ID of the first square in a variable --
         let firstCheck = totalSquares[0].id;
 
+        // Puts the ID of the targeted Square in a variable for easier function passing --
         let squareId = newSquare.id;
+        //Puts the Square object in a variable for easier function passing --
         let kill = newSquare;
 
+        // Even or Odd check function. 0 is Even and else is odd --
         let a = squareId % 2;
         if (a == 0) {
-            console.log("kill Even")
             removeNextSquare(squareId, lastCheck, kill);
         } else {
-            console.log("kill Odd")
             removePreviousSquare(squareId, firstCheck, kill);
         }
     });
 
+    // Increments squareNum for the next addSquare() call --
     squareNum++;
 }
-
-function bgSwap() {
-    let a = Math.floor(Math.random() * colors.length); // Random number between 0 and 8 --
-    return colors[a];
-}
-
-let colors = ["red", "blue", "green", "yellow", "pink", "orange", "brown"];
 
 function removeNextSquare(squareId, lastCheck, kill) {
     if (squareId == lastCheck) {
@@ -94,14 +100,16 @@ function removeNextSquare(squareId, lastCheck, kill) {
 }
 
 function removePreviousSquare(squareId, firstCheck, kill) {
-    if (squareId == firstCheck){
+    if (squareId == firstCheck) {
         alert("This is the first Square. \nCan not remove previous.");
     } else {
         kill.previousElementSibling.remove();
     }
 }
 
-// Add 10 squares for prototyping --
-for (let index = 0; index < 10; index++) {
-    addSquare();
+// Changes the background color of the targeted square --
+let colors = ["red", "blue", "green", "yellow", "pink", "orange", "brown"];
+function bgSwap() {
+    let a = Math.floor(Math.random() * colors.length); // Random number between 0 and 8 --
+    return colors[a];
 }
